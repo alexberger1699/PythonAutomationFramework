73e6d17be76de402f1389792304ab21bd30d6fe1
alexberger1699 <alexberger1699@gmail.com>
2023-07-30 15:18:09 +0300

    add base url and my_account_url

--- autoTests/src/helpers/config_helpers.py
+++ autoTests/src/helpers/config_helpers.py
@@ -2,3 +2,5 @@ import os
 
 def get_base_url():
     env = os.environ.get('ENV', 'test')
+    if env.lower()== 'test':
+        return 'https://supersqa.com'
--- autoTests/src/pages/MyAccountSignedOut.py
+++ autoTests/src/pages/MyAccountSignedOut.py
@@ -2,14 +2,21 @@ from autoTests.src.pages.locators import MyAccountSinedIOutLocator
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from autoTests.src.SeleniumExtended import SeleniumExtended
+from autoTests.src.helpers.config_helpers import get_base_url
 
 class MyAccountSignedOut(MyAccountSinedIOutLocator):
+
 
     def __init__(self, driver):
         self.driver = driver
         self.sl = SeleniumExtended(self.driver)
+
+
     def go_to_my_account(self):
+        base_url = get_base_url()
+        my_account_url = base_url + self.endpoint
         self.driver.get('')
+
 
     def input_login_username(self, username):
         self.sl.wait_and_input_text(self.LOGIN_USERNAME, username)
